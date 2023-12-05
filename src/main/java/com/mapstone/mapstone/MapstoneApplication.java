7c70eb73b28250a19371d2de5253498810cb7665
Jessica Rosier <jessica.c.rosier1@gmail.com>
2023-12-05 11:39:01 -0600

    initial commit

---
+++ src/main/java/com/mapstone/mapstone/MapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.mapstone.mapstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapstoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapstoneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mapstone/mapstone/MapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mapstone.mapstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapstoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
