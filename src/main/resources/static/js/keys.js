4dbca13cc0bfe3ed3ab8bbe985abee5b695b9927
William Clayton <william.d.clayton1@gmail.com>
2023-12-07 10:04:48 -0600

    -wc

---
+++ src/main/resources/static/js/keys.js
@@ -0,0 +1,4 @@
+export const MAP_BOX_TOKEN = 'pk.eyJ1Ijoid2lsbGMzMzYiLCJhIjoiY2xtZjhpZWMxMDM3cjNlanlxeDVrYm5mNyJ9.7KaiTbt7Gu5zx-7uACDdLg';
+
+export const FILE_STACK_TOKEN = 'AkVrAdtkNT0WfVl1ysWh2z';
+
