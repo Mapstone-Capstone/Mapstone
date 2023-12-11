e667661e2681352062e3366a36b6680ebaae4d9d
Jessica Rosier <jessica.c.rosier1@gmail.com>
2023-12-11 14:50:23 -0600

    created badges model - repository - and js file - badges functional

---
+++ src/main/resources/static/js/badges.js
@@ -0,0 +1,30 @@
+const badge1 = document.getElementById("badge1");
+const badge2 = document.getElementById("badge2");
+const badge3 = document.getElementById("badge3");
+const badge4 = document.getElementById("badge4");
+const badge5 = document.getElementById("badge5");
+
+
+// when badge is clicked, open modal explaining badge
+badge1.addEventListener("click", function () {
+    const badgeModal = document.createElement("div");
+    const badgeName = badgeModal.querySelector(".badge-name");
+    const badgeDescription = badgeModal.querySelector(".badge-description");
+    console.log(badgeName.value);
+    badgeModal.classList.add("modal");
+    badgeModal.innerHTML = `
+    <div class="modal-bg"></div>
+        <div class="modal-content">
+            <span class="close">&times;</span>
+            <h3>${badgeName.value}</h3>
+            <p>${badgeDescription.value}</p>
+        </div>
+    `;
+    badgeModal.style.display = "block";
+    const span = badgeModal.querySelector(".close");
+    span.addEventListener("click", function () {
+        badgeModal.style.display = "none";
+    });
+
+    document.body.appendChild(badgeModal);
+}
