3673164e74c6f33568ab35b92fdc1976d8f23879
Jessica Rosier <jessica.c.rosier1@gmail.com>
2023-12-13 16:45:01 -0600

    jr

--- src/main/resources/static/js/edit-profile.js
+++ src/main/resources/static/js/edit-profile.js
@@ -33,50 +33,48 @@ async function postUpdatedUser(id) {
 
 
 
-async function getBadges(mapId) {
-    const badges = await getBadgesByMapId(mapId);
-    console.log(badges)
-    badges.forEach((badge) => {
-        const badgeDiv = document.createElement("div");
-        badgeDiv.classList.add("badge-wrapper");
-        badgeDiv.innerHTML = `
-        <img class="rounded-circle badge-img" value="${badge.url}" src="${badge.url}" alt="badge"/>
-        <p hidden="hidden" id="badge${badge.id}"></p>
-        <p hidden="hidden" class="badge-name">${badge.name}</p>
-        <p hidden="hidden"class="badge-description">${badge.description}</p>
-    </p>`;
-
-        const thisBadge = badgeDiv.querySelector(".badge-img");
-        thisBadge.addEventListener("click", function () {
-            const badgeModal = document.createElement("div");
-
-            badgeModal.classList.add("modal");
-            badgeModal.innerHTML = `
-            <div class="modal-bg"></div>
-                <div class="modal-content d-flex justify-content-center align-items-center">
-                    <span class="modal-close">&times;</span>
-                    <h3>${badge.name}</h3>
-                    <img class="rounded-circle badge-img" src="${badge.url}" alt="badge"/>
-                    <p>${badge.description}</p>
-                </div>
-            `;
-            const modalClose = badgeModal.querySelector(".modal-close");
-            modalClose.addEventListener("click", function () {
-                badgeModal.remove();
-            });
-
-            const modalBg = badgeModal.querySelector(".modal-bg");
-            modalBg.addEventListener("click", function () {
-                badgeModal.remove();
-            });
-
-            document.body.appendChild(badgeModal);
-        });
-
-
-        badgeContainer.appendChild(badgeDiv);
-    });
-}
+// async function updateUser(mapId) {
+//     // const badges = await getBadgesByMapId(mapId);
+//     console.log(badges)
+//     badges.forEach((badge) => {
+//         const badgeDiv = document.createElement("div");
+//         badgeDiv.classList.add("badge-wrapper");
+//         badgeDiv.innerHTML = `
+//         <img class="rounded-circle badge-img" value="${badge.url}" src="${badge.url}" alt="badge"/>
+//         <p hidden="hidden" id="badge${badge.id}"></p>
+//         <p hidden="hidden" class="badge-name">${badge.name}</p>
+//         <p hidden="hidden"class="badge-description">${badge.description}</p>
+//     </p>`;
+//
+//         const thisBadge = badgeDiv.querySelector(".badge-img");
+//         thisBadge.addEventListener("click", function () {
+//             const badgeModal = document.createElement("div");
+//
+//             badgeModal.classList.add("modal");
+//             badgeModal.innerHTML = `
+//             <div class="modal-bg"></div>
+//                 <div class="modal-content d-flex justify-content-center align-items-center">
+//                     <span class="modal-close">&times;</span>
+//                     <h3>${badge.name}</h3>
+//                     <img class="rounded-circle badge-img" src="${badge.url}" alt="badge"/>
+//                     <p>${badge.description}</p>
+//                 </div>
+//             `;
+//             const modalClose = badgeModal.querySelector(".modal-close");
+//             modalClose.addEventListener("click", function () {
+//                 badgeModal.remove();
+//             });
+//
+//             const modalBg = badgeModal.querySelector(".modal-bg");
+//             modalBg.addEventListener("click", function () {
+//                 badgeModal.remove();
+//             });
+//
+//
+//         });
+//
+//     });
+// }
 
 getBadges(mapId).then(() => {
     console.log("Badges loaded");
