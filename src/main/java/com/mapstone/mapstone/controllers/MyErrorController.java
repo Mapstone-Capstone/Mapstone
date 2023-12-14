9c2269e11f1423df7649450b81073f7b73a113dc
Clifford Gardner <cliffordgardner9@gmail.com>
2023-12-14 16:40:06 -0600

    test

--- src/main/java/com/mapstone/mapstone/controllers/MyErrorController.java
+++ src/main/java/com/mapstone/mapstone/controllers/MyErrorController.java
@@ -18,15 +18,16 @@ public class MyErrorController implements ErrorController {
 
         if (status != null) {
             Integer statusCode = Integer.valueOf(status.toString());
-
             if(statusCode == HttpStatus.NOT_FOUND.value()) {
-                System.out.println("Not found");
+                System.out.println("\n\n\n\n\nNot found\n\n\n\n\n");
                 return "error/4xx";
-            }else if (statusCode == HttpStatus.FORBIDDEN.value()){
-                System.out.println("forbidden");
+            } else if (statusCode == HttpStatus.BAD_REQUEST.value()) {
+                System.out.println("\n\n\n\n\nBad Request\n\n\n\n\n");
+            } else if (statusCode == HttpStatus.FORBIDDEN.value()){
+                System.out.println("\n\n\n\n\nforbidden\n\n\n\n\n\n");
                 return "/error/403";
             } else if (statusCode == HttpStatus.METHOD_NOT_ALLOWED.value()) {
-                System.out.println("not working");
+                System.out.println("\n\n\n\n\nnot working\n\n\n\n\n");
                 return "error/405";
             }
         }
--- src/main/resources/static/js/sharelink.js
+++ src/main/resources/static/js/sharelink.js
@@ -3,13 +3,22 @@ let id = document.getElementById("share").value
 let text = document.getElementById("link")
 let modal = document.getElementById("sharelinkmodal")
 let close = document.getElementsByClassName("close")[0]
-
+let facebook = document.getElementById("facebook")
+let linkedin = document.getElementById("linkedin")
+let twiiter = document.getElementById("twitter")
 Sharelink.addEventListener("click", async (event)=>{
     modal.style.display="block";
-    let link = "localhost:8080/viewprofile/" + id;
+    let link = "https://www.map-share.net/viewprofile/" + id;
     // copies info to the clipboard HOWEVER IT WILL BE A PROMISE
     await navigator.clipboard.writeText(link)
+
+    let attribute= "window.open('https://www.facebook.com/share.php?u="+link+"','popup','width=600,height=600'); return false;"
+    let attribute2 = "window.open('https://www.linkedin.com/sharing/share-offsite/?url="+link+"','popup','width=600,height=600'); return false;"
+    let attribute3 = "window.open('https://twitter.com/intent/tweet?url="+link+"','popup','width=600,height=600'); return false;"
     text.value=link;
+    facebook.setAttribute("onclick",attribute)
+    linkedin.setAttribute("onclick",attribute2)
+    twiiter.setAttribute("onclick",attribute3)
     // alert("Share link copied: "+ link)
 })
 
