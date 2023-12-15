121bfce358c672a092253dab9405b4a7f5fa6183
William Clayton <william.d.clayton1@gmail.com>
2023-12-15 11:12:39 -0600

    +-wc

--- src/main/resources/static/js/edit-journal.js
+++ src/main/resources/static/js/edit-journal.js
@@ -1,8 +1,8 @@
 // import {getAllEntries} from "./mapbox-map-utils";
-//
-//
-// const editJournalBtn = document.getElementById('edit-journal-btn');
-//
+
+
+const editJournalBtn = document.getElementById('edit-journal-btn');
+
 // const usersEntries = () => {
 //
 //     const userId = document.getElementById('all-images');
@@ -48,70 +48,70 @@
 //     });
 //
 // }
-//
-// const editModalPopup = () => {
-//
-//     const editJournalModal = document.createElement("div");
-//     editJournalModal.classList.add("modal");
-//     editJournalModal.setAttribute("id", "edit-journal-modal");
-//
-//     editJournalModal.innerHTML =  `
-//     <div class="modal-bg"></div>
-//
-//     <div class="modal-content">
-//
-//         <div class="modal-header">
-//             <h2 class="modal-title">Edit your Journal</h2>
-//             <span class="modal-close">&times;</span>
-//         </div>
-//         <div id="edit-journal-body" class="modal-body">
-//             <div>
-//                 <button id="create-journal-entry" class="button">Create an Entry</button>
-//             </div>
-//
-//             <div>
-//                 <button id="edit-journal-entry" class="button"> Edit an Entry</button>
-//             </div>
-//
-//             <div>
-//                 <button id="delete-journal-entry" class="button">Delete an Entry</button>
-//             </div>
-//         </div>
-//     </div>
-// `
-//
-//     const modalClose = editJournalModal.querySelector(".modal-close");
-//     const modalBackground = editJournalModal.querySelector(".modal-bg");
-//     const createEntry = editJournalModal.querySelector("#create-journal-entry");
-//     const editEntry = editJournalModal.querySelector("#edit-journal-entry");
-//     const deleteEntry = editJournalModal.querySelector("#delete-journal-entry");
-//
-//     //close modal
-//     modalClose.addEventListener("click", () => {
-//         editJournalModal.remove();
-//     });
-//     modalBackground.addEventListener("click", () => {
-//         editJournalModal.remove();
-//     });
-//
-//     //redirect to create entries page
-//     createEntry.addEventListener("click", () => {
-//         window.location = '/create-entries';
-//     });
-//
-//     //popup edit entry modal
-//     editEntry.addEventListener("click", () => {
-//
-//     });
-//
-//     //popup delete entry modal
-//     deleteEntry.addEventListener("click", () => {
-//
-//     })
-//
-//
-//     document.body.appendChild(editJournalModal);
-//
-// }
-//
-// editJournalBtn.addEventListener('click', editModalPopup);
+
+const editModalPopup = () => {
+
+    const editJournalModal = document.createElement("div");
+    editJournalModal.classList.add("modal");
+    editJournalModal.setAttribute("id", "edit-journal-modal");
+
+    editJournalModal.innerHTML =  `
+    <div class="modal-bg"></div>
+
+    <div class="modal-content">
+
+        <div class="modal-header">
+            <h2 class="modal-title">Edit your Journal</h2>
+            <span class="modal-close">&times;</span>
+        </div>
+        <div id="edit-journal-body" class="modal-body">
+            <div>
+                <button id="create-journal-entry" class="button">Create an Entry</button>
+            </div>
+
+<!--            <div>-->
+<!--                <button id="edit-journal-entry" class="button"> Edit an Entry</button>-->
+<!--            </div>-->
+
+<!--            <div>-->
+<!--                <button id="delete-journal-entry" class="button">Delete an Entry</button>-->
+<!--            </div>-->
+        </div>
+    </div>
+`
+
+    const modalClose = editJournalModal.querySelector(".modal-close");
+    const modalBackground = editJournalModal.querySelector(".modal-bg");
+    const createEntry = editJournalModal.querySelector("#create-journal-entry");
+    const editEntry = editJournalModal.querySelector("#edit-journal-entry");
+    const deleteEntry = editJournalModal.querySelector("#delete-journal-entry");
+
+    //close modal
+    modalClose.addEventListener("click", () => {
+        editJournalModal.remove();
+    });
+    modalBackground.addEventListener("click", () => {
+        editJournalModal.remove();
+    });
+
+    //redirect to create entries page
+    createEntry.addEventListener("click", () => {
+        window.location = '/create-entries';
+    });
+
+    //popup edit entry modal
+    editEntry.addEventListener("click", () => {
+
+    });
+
+    //popup delete entry modal
+    deleteEntry.addEventListener("click", () => {
+
+    })
+
+
+    document.body.appendChild(editJournalModal);
+
+}
+
+editJournalBtn.addEventListener('click', editModalPopup);
