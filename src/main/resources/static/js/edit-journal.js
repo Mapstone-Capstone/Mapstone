cf3a429fc7d99fe2eb88c35e1be210447e4a974c
William Clayton <william.d.clayton1@gmail.com>
2023-12-15 15:33:48 -0600

    -wc

--- src/main/resources/static/js/edit-journal.js
+++ src/main/resources/static/js/edit-journal.js
@@ -1,5 +1,4 @@
-import {getAllEntries} from "./mapbox-map-utils";
-
+import {getAllEntries} from "./mapbox-map-utils.js";
 
 const editJournalBtn = document.getElementById('edit-journal-btn');
 
@@ -9,8 +8,6 @@ const usersEntries = () => {
     const userEntries = document.createElement("div");
     userEntries.classList.add("modal");
     userEntries.setAttribute("id", "user-entries");
-
-
 
     userEntries.innerHTML = `
         <div class="modal-bg"></div>
@@ -26,29 +23,86 @@ const usersEntries = () => {
         </div>
     `;
 
-    const modalBody = userEntries.querySelector('.modal-body');
+    const modalClose = userEntries.querySelector(".modal-close");
+    const modalBackground = userEntries.querySelector(".modal-bg");
 
-    getAllEntries(userId.value).then(function (response) {
-        response.forEach((entry) => {
-
-            modalBody.innerHTML += `
-            <button class="entry-title" type="button">${entry.title}</button>
-        `;
-            let entryBtn = usersEntries.querySelector(".entry-title");
-
-            entryBtn.forEach((btn) => {
-
-                btn.addEventListener("click", () => {
-                    //pull up edit modal
-                });
-
-            });
-
-        });
+    //close modal
+    modalClose.addEventListener("click", () => {
+        userEntries.remove();
+    });
+    modalBackground.addEventListener("click", () => {
+        userEntries.remove();
     });
 
-}
+    const modalBody = userEntries.querySelector('.modal-body');
+    getAllEntries(userId.value).then(function (response) {
 
+        for (let i = 0; i < response.length; i++) {
+            // console.log(response[i]);
+
+            modalBody.innerHTML += `
+                <button class="entry-title" type="button" value="${response[i].id}">${response[i].title}</button>
+            `;
+
+            //use the value of the id to get the json response and add them to a form file
+
+            console.log("I am response:");
+            console.log(response[i]);
+            const entryTitle = modalBody.querySelectorAll('.entry-title');
+            console.log(entryTitle);
+            entryTitle.forEach((elementBtn) => {
+                elementBtn.addEventListener('click', () => {
+
+                    const userEntry = document.createElement("div");
+                    userEntry.classList.add("modal");
+                    userEntry.setAttribute("id", "user-entry");
+
+                    userEntry.innerHTML = `
+                    <div class="modal-bg"></div>
+
+                    <div class="modal-content">
+
+                        <div class="modal-header">
+                            <h2 class="modal-title">Pick an Entry to edit</h2>
+                            <span class="modal-close">&times;</span>
+                        </div>
+                        <div id="user-entries-body" class="modal-body">
+                            <input value="${response[i].title}" />
+                            <input value="${response[i].date}" type="date" />
+                            <input value="${response[i].description}" />
+                        </div>
+                        <div class="modal-footer">
+                            <button id="close" type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button id="save-changes" type="button" class="btn btn-primary">Save changes</button>
+                        </div>
+                    </div>
+                `;
+
+                    const modalClose = userEntry.querySelector(".modal-close");
+                    const modalBackground = userEntry.querySelector(".modal-bg");
+                    const close = userEntry.querySelector('#close');
+
+                    //close modal
+                    modalClose.addEventListener("click", () => {
+                        userEntry.remove();
+                    });
+                    modalBackground.addEventListener("click", () => {
+                        userEntry.remove();
+                    });
+                    close.addEventListener("click", () => {
+                        userEntry.remove();
+                    });
+
+                    document.body.appendChild(userEntry);
+                });
+            });
+
+
+
+            }
+        document.body.appendChild(userEntries);
+    });
+}
 const editModalPopup = () => {
 
     const editJournalModal = document.createElement("div");
@@ -100,18 +154,18 @@ const editModalPopup = () => {
     });
 
     //popup edit entry modal
-    editEntry.addEventListener("click", () => {
-
-    });
+    editEntry.addEventListener("click", usersEntries);
 
     //popup delete entry modal
     deleteEntry.addEventListener("click", () => {
 
     })
 
-
     document.body.appendChild(editJournalModal);
+
+
 
 }
 
 editJournalBtn.addEventListener('click', editModalPopup);
+
