576d6e10f38c361ee5ea65a7e1ff8bab72de5e9f
Jessica Rosier <jessica.c.rosier1@gmail.com>
2023-12-18 11:01:57 -0600

    added accepted filetypes to fs picker options

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -32,13 +32,14 @@ if (firstTime === "true") {
             {element: '#map', popover: {title: 'Map', description: 'Begin by exploring the map. Click or tap on any country you\'ve visited. Once selected, the country will be filled with a vibrant color, indicating your travel adventure.', side: 'left', align: 'center'}},
             {element: '#search-input', popover: {title: 'Search', description: 'Not good with geography? Try searching for a country by name and let us help you find it!', position: 'bottom'}},
             {element: '#open-update-modal', popover: {title: 'We\'ve got style!', description: 'Click here to customize the look of your map. Choose what suits you best!', position: 'bottom'}},
-            {element: '#share', popover: {title: 'Share with fiends!', description: 'Now that you\'ve documented your travels, it\'s time to share your personalized travel map with friends and fellow adventurers. Click on the "Share Map" button to spread the joy of your globetrotting experiences on social media.', position: 'bottom'}},
-            {element: '#some-element', popover: {title: 'That"s all for now!', description: 'Congratulations! You\'re ready to start documenting your travels on the map and create a visual diary of your adventures. Explore more countries, upload more photos, and let your travel map tell the story of your incredible journey.\n' +
+            {element: '#share', popover: {title: 'Share with fiends!', description: "Now that you\'ve documented your travels, it\'s time to share your personalized travel map with friends and fellow adventurers. Click on the 'Share Map' button to spread the joy of your globetrotting experiences on social media.", position: 'bottom'}},
+            {element: '#some-element', popover: {title: "That's all for now!", description: 'Congratulations! You\'re ready to start documenting your travels to create a visual diary of your adventures. Explore more countries, upload more photos, and let your travel map tell the story of your journey.\n' +
                         '\n' +
                         'Happy exploring! 🌍✈️', position: 'bottom'}},
 
         ]
     });
+
     // start the tutorial but only show it once
     if (!seenTutorial) {
         driverObj.drive()
