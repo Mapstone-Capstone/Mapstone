a2e2530e7d69cbeed362c42cbea69982cc055088
Jessica Rosier <jessica.c.rosier1@gmail.com>
2023-12-17 19:22:19 -0600

    removed all images and replaces img srcs with fs link

--- src/main/java/com/mapstone/mapstone/sql/badges_seeder.sql
+++ src/main/java/com/mapstone/mapstone/sql/badges_seeder.sql
@@ -1,9 +1,9 @@
 USE maps_db;
 
 INSERT into badges (id, bagde_url,name, description)
-VALUES (1, '/assets/badges/on-the-map.svg', 'On the Map', 'You visited your first country!'),
-       (2, '/assets/badges/globe-trotter.svg', 'Globe Trotter', 'You visited 1 country on every continent!'),
-       (3, '/assets/badges/pangea-pioneer.svg', 'Pangea Pioneer', 'I need to think of a description for this badge'),
-       (4, '/assets/badges/5.svg', '5', 'You visited 5 countries!'),
-       (5, '/assets/badges/10.svg', '10', 'You visited 10 countries!');
+VALUES (1, '/images/badges/on-the-map.svg', 'On the Map', 'You visited your first country!'),
+       (2, '/images/badges/globe-trotter.svg', 'Globe Trotter', 'You visited 1 country on every continent!'),
+       (3, '/images/badges/pangea-pioneer.svg', 'Pangea Pioneer', 'I need to think of a description for this badge'),
+       (4, '/images/badges/5.svg', '5', 'You visited 5 countries!'),
+       (5, '/images/badges/10.svg', '10', 'You visited 10 countries!');
 
