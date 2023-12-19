e71551bf27a68948b69085bcf1b236a0b333023c
Jessica Rosier <jessica.c.rosier1@gmail.com>
2023-12-18 21:03:30 -0600

    added more achievments to badges seeder

--- src/main/java/com/mapstone/mapstone/sql/badges_seeder.sql
+++ src/main/java/com/mapstone/mapstone/sql/badges_seeder.sql
@@ -1,9 +1,21 @@
 USE maps_db;
 
-INSERT into badges (id, bagde_url,name, description)
+INSERT into badges (id, bagde_url, name, description)
 VALUES (1, '/assets/badges/on-the-map.svg', 'On the Map', 'You visited your first country!'),
        (2, '/assets/badges/globe-trotter.svg', 'Globe Trotter', 'You visited 1 country on every continent!'),
-       (3, '/assets/badges/pangea-pioneer.svg', 'Pangea Pioneer', 'I need to think of a description for this badge'),
+       (3, '/assets/badges/americas-adventurer.svg', 'Americas Adventurer',
+        'You visited 1 country in North America and 1 country in South America!'),
        (4, '/assets/badges/5.svg', '5', 'You visited 5 countries!'),
-       (5, '/assets/badges/10.svg', '10', 'You visited 10 countries!');
+       (5, '/assets/badges/10.svg', '10', 'You visited 10 countries!'),
+       (6, '/assets/badges/euro-trekker.svg', 'Euro-Trekker', 'You visited 5 countries in Europe!'),
+       (7, '/assets/badges/asia-explorer.svg', 'Asia Explorer', 'You visited 5 countries in Asia!'),
+       (8, '/assets/badges/africa-adventurer.svg', 'Africa Adventurer', 'You visited 5 countries in Africa!'),
+       (9, '/assets/badges/north-america-navigator.svg', 'North America Navigator',
+        'You visited 5 countries in North America!'),
+       (10, '/assets/badges/south-america-surveyor.svg', 'South America Surveyor',
+        'You visited 5 countries in South America!'),
+       (11, '/assets/badges/island-hopper.svg', 'Island Hopper', 'You visited 5 countries on islands!'),
+       (12, '/assets/badges/pangea-pioneer.svg', 'Pangea Pioneer', 'You collected all the badges!');
+
+
 
