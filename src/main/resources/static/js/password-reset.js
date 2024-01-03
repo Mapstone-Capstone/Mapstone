4ccd61891e4a79023b56802c0bd617a0509bd091
Jessica Rosier <jessica.c.rosier1@gmail.com>
2024-01-03 13:25:52 -0600

    password requirments reinstated

--- src/main/resources/static/js/password-reset.js
+++ src/main/resources/static/js/password-reset.js
@@ -1,28 +1,46 @@
 const newPassword = document.getElementById("password");
 const newPasswordConfirm = document.getElementById("confirm");
 let passwordValidation = document.getElementById("password-validation");
-
+const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@.#$!%*?&^])[A-Za-z\d@.#$!%*?&]{8,15}$/;
 const form = document.getElementById("password-reset-form");
-const submitBtn = document.getElementById("submit-btn");
 
 let validated = false;
+let passwordComplex = false;
 
-newPasswordConfirm.addEventListener("keyup", () => {
-    if (newPassword.value !== newPasswordConfirm.value) {
-        passwordValidation.innerHTML = "Passwords do not match";
-        validated = false;
+//validate that the password meets the regex requirements as the user types AND that the password and password confirm fields match
+newPassword.addEventListener("input", (e) => {
+    e.preventDefault();
+    if (newPassword.value.length < 8 || !regex.test(password.value)) {
+        newPassword.classList.add("invalid");
+        passwordComplex = false;
+        passwordValidation.innerText = "Password must be at least 8 characters long, contain one uppercase, one number, and one special character.";
     } else {
-        passwordValidation.innerHTML = "";
-        validated = true;
+        newPassword.classList.remove("invalid");
+        passwordComplex = true;
+        passwordValidation.innerText = "";
+        checkPassword();
     }
+
+
 });
+
+
+
+const checkPassword = () => {
+    newPasswordConfirm.addEventListener("keyup", () => {
+        if (newPassword.value !== newPasswordConfirm.value) {
+            passwordValidation.innerHTML = "Passwords do not match";
+            validated = false;
+        } else {
+            passwordValidation.innerHTML = "";
+            validated = true;
+        }
+    });
+}
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    if (!validated) {
-        passwordValidation.innerHTML = "Passwords do not match";
-    } else {
-
+    if (validated && passwordComplex) {
         form.submit();
     }
 });
